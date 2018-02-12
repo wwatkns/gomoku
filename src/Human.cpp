9b147fc49c7c60ea75614f7ce63c51ae708fbbfb
moannuo <demenet.alain@hotmail.fr>
2018-02-12 17:06:37 +0100

    Add check_action

--- src/Human.cpp
+++ src/Human.cpp
@@ -21,13 +21,18 @@ t_action    Human::play(void) {
 
     while (true) {
         this->_gui->update_events();
-        if (this->_gui->check_mouse_action() || this->_gui->check_close())
+        if (this->_gui->check_mouse_action()) {
+            action.pos = this->_gui->screen_to_grid(this->_gui->get_mouse_pos());
+            action.player_id = this->_id;
+            // action.player = this;
+            action.id = this->_game_engine->get_history_size() + 1;
+            action.timepoint = std::chrono::steady_clock::now() - this->_game_engine->get_initial_timepoint();
+            if (this->_game_engine->check_action(action))
+                break;
+        }
+        if (this->_gui->check_close())
             break;
         this->_gui->update_display();
     }
-    action.pos = this->_gui->screen_to_grid(this->_gui->get_mouse_pos());
-    action.player_id = this->_id;
-    action.id = this->_game_engine->get_history_size() + 1;
-    action.timepoint = std::chrono::steady_clock::now() - this->_game_engine->get_initial_timepoint();
     return action;
 }
